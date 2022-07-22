f5e94725683e439658e9879f755d7cd6f6da7e82
Virjanand Panday <virjanand@virjanand.com>
2022-07-22 13:22:35 +0200

    Green test: convert 2 to II

--- src/main/java/roman/RomanNumber.java
+++ src/main/java/roman/RomanNumber.java
@@ -1,10 +1,16 @@
 package roman;
 
 public class RomanNumber {
+    private int arabicNumber;
+
     public RomanNumber(int arabicNumber) {
+        this.arabicNumber = arabicNumber;
     }
 
     public String getRoman() {
+        if (arabicNumber == 2) {
+            return "II";
+        }
         return "I";
     }
 }
